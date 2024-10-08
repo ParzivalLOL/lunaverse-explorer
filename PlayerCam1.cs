6bfb5e068da30c3e30efafd2a46cb9b6339b98f1
Abheek Shukla <86312982+ParzivalLOL@users.noreply.github.com>
2024-10-08 17:51:59 +0500

    Add files via upload

---
+++ PlayerCam1.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam1 : MonoBehaviour
+{
+    public float sensitivityX;
+    public float sensitivityY;
+
+    public Transform orientation;
+
+    float xRot;
+    float yRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+
+        yRot += mouseX;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        orientation.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+}
